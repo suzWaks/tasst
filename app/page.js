2efaa117a096d670675161dc5b86feb54c6b10a8
suzWaks <suzalwakhley@gmail.com>
2024-02-15 23:30:19 +0600

    Media query

--- app/page.js
+++ app/page.js
@@ -6,15 +6,17 @@ import Footer from '@/components/Footer/page'
 export default function HomePage() {
   return (
     <div>
-      <div className={styles.heroText}>
-        <h1>THE TASST FAMILY</h1>
-        <h3>Happy family of five</h3>
-      </div>
-      <div className={styles.image}>
-        <Image src='/family.jpg' fill alt='family pic' />
-      </div>
-      <div className={styles.heading}>
-        <h1>KNOW ABOUT US MORE</h1>
+      <div className={styles.heroSection}>
+        <div className={styles.heroText}>
+          <h1>THE TASST FAMILY</h1>
+          <h3>Happy family of five</h3>
+        </div>
+        <div className={styles.image}>
+          <Image src='/family.jpg' fill alt='family pic' />
+        </div>
+        <div className={styles.heading}>
+          <h1>KNOW ABOUT US MORE</h1>
+        </div>
       </div>
 
       <IndividualBrief />
