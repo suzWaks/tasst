af92530b4204a6518081997e3e2b637a50e3857f
suzWaks <suzalwakhley@gmail.com>
2024-02-16 00:31:09 +0600

    Added framer-motion for animations

--- app/page.js
+++ app/page.js
@@ -1,16 +1,22 @@
+'use client'
+
 import Image from 'next/image'
 import styles from './page.module.css'
 import IndividualBrief from '@/components/HomePage/IndividualBrief/page'
 import ResidenceBanner from '@/components/HomePage/ResidenceBanner/page'
 import Footer from '@/components/Footer/page'
+import { motion, AnimatePresence } from "framer-motion"
+
 export default function HomePage() {
   return (
     <div>
       <div className={styles.heroSection}>
-        <div className={styles.heroText}>
+        <motion.div initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }} className={styles.heroText}>
           <h1>THE TASST FAMILY</h1>
           <h3>Happy family of five</h3>
-        </div>
+        </motion.div>
         <div className={styles.image}>
           <Image src='/family.jpg' fill alt='family pic' />
         </div>
