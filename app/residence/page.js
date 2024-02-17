ace4be6b4196d5930654e58cb4086dff2d672f80
suzWaks <suzalwakhley@gmail.com>
2024-02-17 21:31:38 +0600

    Fixed IndividualBrief

--- app/residence/page.js
+++ app/residence/page.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Residence() {
   return (
-    <div>residence</div>
+    <div>Residence</div>
   )
 }
--- components/HomePage/IndividualBrief/page.js
+++ components/HomePage/IndividualBrief/page.js
@@ -1,71 +1,59 @@
+import styles from './page.module.css'
 import Image from 'next/image';
-import styles from './page.module.css';
-import ChevronRight from '../../Icons/Chevronright';
 import { details } from '@/data';
-import React from 'react'
 import Button from '@/components/ui/button';
-import { motion } from 'framer-motion';
+import * as motion from '@/components/HomePage/IndividualBrief/motion'
+
+import React from 'react';
 
 export default function IndividualBrief() {
     return (
-
         details.map(data =>
             <motion.div initial={{ opacity: 0, scale: 0.5 }}
                 whileInView={{ opacity: 1, scale: 1 }}
-                viewport={{ once: true }} transition={{ ease: "easeOut", duration: 1 }}
-                className={styles.container} key={data.id}>
+                viewport={{ once: true }} transition={{ ease: "easeOut", duration: 1 }} className={styles.container} key={data.id}>
+
                 {data.id % 2 === 0 ? (
                     <>
-                        <div className={`${styles.column} ${styles.leftColumn}`}>
-
-                            <>
-                                <div key={data.id} className={styles.row1}>{data.name}</div>
-                                <div key={data.id} className={styles.row2}>{data.description.split('\n').map((line, index) => (
-                                    <React.Fragment key={index}>
-                                        {line}
-                                        <br />
-                                    </React.Fragment>
-                                ))}</div>
-                            </>
-
-
-                            <Button text='Know More' />
-                        </div>
                         <div className={styles.column}>
-                            <div className={styles.imageWrapper}>
-                                <Image src={data.image} layout='fill' alt={data.name} />
-                            </div>
+                            <Image src={data.image} width={400} height={400} alt={data.name} />
+                        </div>
+                        <div className={styles.column} >
+                            <h2>{data.name}</h2>
+                            <p>{data.description.split('\n').map((line, index) => (
+                                <React.Fragment key={index}>
+                                    {line}
+                                    <br />
+                                </React.Fragment>
+                            ))}
+                            </p>
+                            <Button text='Know More' />
+
                         </div>
                     </>
 
                 ) : (
                     <>
-                        <div className={styles.column}>
-                            <div className={styles.imageWrapper}>
-                                <Image src={data.image} layout="fill" alt={data.name} />
-                            </div>
+                        <div className={`${styles.column} ${styles.colImg}`}>
+                            <Image src={data.image} width={400} height={400} alt={data.name} />
                         </div>
-                        <div className={`${styles.column} ${styles.leftColumn}`}>
-
-                            <>
-                                <div key={data.id} className={styles.row1}>{data.name}</div>
-                                <div key={data.id} className={styles.row2}>{data.description.split('\n').map((line, index) => (
-                                    <React.Fragment key={index}>
-                                        {line}
-                                        <br />
-                                    </React.Fragment>
-                                ))}</div>
-                            </>
-
-
+                        <div className={`${styles.column} ${styles.coltext}`}>
+                            <h2>{data.name}</h2>
+                            <p>{data.description.split('\n').map((line, index) => (
+                                <React.Fragment key={index}>
+                                    {line}
+                                    <br />
+                                </React.Fragment>
+                            ))}
+                            </p>
                             <Button text='Know More' />
                         </div>
 
                     </>
                 )}
-
             </motion.div>
+
         )
 
-    );
+    )
 }
